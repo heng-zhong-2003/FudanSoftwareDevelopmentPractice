49d22fedf57fca76e961bbbbfd2e5ccc2f5163f8
Fragrance Shang <2869528466@qq.com>
2024-10-21 23:42:08 +0800

    change models

--- models.py
+++ models.py
@@ -9,7 +9,7 @@ class UserModel(db.Model):
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     username = db.Column(db.String(50), nullable=False)
-    password = db.Column(db.String(100), nullable=False)
+    password = db.Column(db.String(200), nullable=False)
     email = db.Column(db.String(50), nullable=False, unique=True)
     
 class ProjectModel(db.Model):
