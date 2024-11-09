17ab8c6973dd6cd230d0ba3be84652e7cfbd5ffe
joannegxy <115196827+joannegxy@users.noreply.github.com>
2024-11-09 21:46:53 +0800

    Update config.py

--- config.py
+++ config.py
@@ -3,9 +3,9 @@ SECRET_KEY = ".yycsieovltsrifym;"
 
 HOSTNAME = '127.0.0.1'
 PORT = '3306'
-DATABASE = 'software_architecture'
+DATABASE = 'research_project'
 USERNAME = 'root'
-PASSWORD = 'Joanne1428'
+PASSWORD = '******'
 DB_URI = 'mysql+pymysql://{}:{}@{}:{}/{}?charset=utf8'.format(USERNAME, PASSWORD, HOSTNAME, PORT, DATABASE)
 SQLALCHEMY_DATABASE_URI = DB_URI
 
